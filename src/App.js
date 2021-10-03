64e67efa436899800061e8d305c2bd15eb831dda
Alphonse KAMAN <kamanalphonse@gmail.com>
2021-10-03 17:34:10 +0100

    Navbar update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+// import components
 import Navbar from './components/navbar';
+import Header from './components/header';
+import Particules from 'react-particles-js'
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-    </div>
+    <>
+      <Particules 
+        className='tsparticles-canvas-el'
+        params={{
+          particules: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900
+              }
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 6,
+                color: "#f9ab00"
+              }
+            }
+          }
+        }}
+      />
+      <Navbar />
+      <Header />
+    </>
   );
 }
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,15 +1,14 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react';
 import alexLogo from '../img/logo.PNG'
 const Navbar = () => {
     
     return (
-    <nav className="navbar navbar-expand-lg fixed-top">
+    <nav className="navbar navbar-expand-lg">
             <div className='container'>
             <a className="navbar-brand" href="#"><img className='logo' src={alexLogo} alt='logo...'/></a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav">
                     <li className="nav-item active">
